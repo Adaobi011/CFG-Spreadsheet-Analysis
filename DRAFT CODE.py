d935ad9e515454f0c43657394015f9bdf8920ebb
olscuration <aoogunyemi@icloud.com>
2022-08-15 19:31:15 +0100

    Does these files show up?

---
+++ DRAFT CODE.py
@@ -0,0 +1,14 @@
+#reading in csv file
+
+df = pd.read_csv('sales.csv')
+df.name = 'sales'
+print(df.to_string())
+list_sales = df['sales'].tolist()
+total_sales = sum(list_sales)
+
+#outputting the total sales across all months
+
+sales_month = list(df['sales'])
+print(sales_month)
+total_month = sum(sales_month)
+print(total_month)
