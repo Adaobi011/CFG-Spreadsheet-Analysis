bf32efca641018ed1cc84c5c3f4d2ee1a669cd30
Adaobi011 <adaobi@hotmail.co.uk>
2022-08-15 19:04:16 +0100

    CFG Project

---
+++ Sales Project.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# reading in csv file
+
+df = pd.read_csv('sales.csv')
+df.name = 'sales'
+print(df.to_string())
+list_sales = df['sales'].tolist()
+total_sales = sum(list_sales)
+
+# Outputting the total sales across all months
+
+sales_month = list(df['sales'])
+print(sales_month)
+total_month = sum(sales_month)
+print(total_month)
+
+# data visualisation
+
+import matplotlib.pyplot as plt
+
+
+
+
+
